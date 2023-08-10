9dad6bc74636876868d85675f42073c2b24a730b
AIKP (Ari Siggaard Knoph) <aikp@novonordisk.com>
2023-08-10 09:16:54 +0200

    Remove website activity at the bottom of landing page

--- scripts/add_contributions.R
+++ scripts/add_contributions.R
@@ -156,12 +156,6 @@ cat(
     "
     {html_wg}
     
-    </br>
-    <h4>Website</h4>
-    <p>Activity on the website and metadata is scraped automatically based on 
-    contributions to the codebase <a href='https://github.com/orgs/pharmaverse/projects/3'>(to contribute click here)</a>
-    {html_website}
-    </br>
     "
   )
   ,
