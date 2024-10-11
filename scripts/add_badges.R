3985960fc250a6a7d2d197bfdc2b14f062fa14da
James Black <james@epijim.uk>
2024-10-11 09:59:35 +0200

    new process for badges

--- scripts/add_badges.R
+++ scripts/add_badges.R
@@ -1,111 +1,26 @@
 library(dplyr)
-library(readr)
+library(arrow)
 library(base64enc)
 library(glue)
-library(shieldR)
-library(gt)
 
-## What files do we have?
+## Get packages ---------
 
-  files_pharmaverse <- paste0("data/packages/",list.files("data/packages"))
+files_pharmaverse <- paste0("data/packages/",list.files("data/packages"))
 
-  files <- c(
-    files_pharmaverse,
-    paste0("data/nonpharma/",list.files("data/nonpharma"))
+## Get badges ---------
+
+badges <- arrow::read_parquet(
+  "https://openpharma.github.io/generate_badges/output/data.parquet"
   )
-  
-  
-## External badges -------------
-  dir_shields <- "static/shields"
-  
-  # check if a pkg has been removed!
-  removed <- unique(c(
-    setdiff(
-      gsub(".png","",list.files(dir_shields, pattern = "*.png")),
-      gsub(pattern = ".yaml",replacement = "",basename(files_pharmaverse))
-    ),
-    setdiff(
-      gsub(".svg","",list.files(dir_shields, pattern = "*.svg")),
-      gsub(pattern = ".yaml",replacement = "",basename(files_pharmaverse))
-    )
-  ))
-  
-  if(length(removed) > 0) {
-    message("The following packages are no longer pharmaverse: ",
-          paste(removed, collapse = ", "))
-    
-    file.remove(
-      glue("{dir_shields}/{removed}.png")
-    )
-    file.remove(
-      glue("{dir_shields}/{removed}.svg")
-    )
-  }
-  
 
-  
-  # check if a pkg already exists
-  missing <- unique(c(
-    setdiff(
-      gsub(pattern = ".yaml",replacement = "",basename(files_pharmaverse)),
-      gsub(".png","",list.files(dir_shields, pattern = "*.png"))
-    ),
-    setdiff(
-      gsub(pattern = ".yaml",replacement = "",basename(files_pharmaverse)),
-      gsub(".svg","",list.files(dir_shields, pattern = "*.svg"))
-    )
-  ))
-  
-  if(length(missing) > 0) {
-    message("The following packages are new to the pharmaverse: ",
-            paste(missing, collapse = ", "))
-  
-    
-    # render new badges
-    for (i in missing) {
-  
-      shield_build(
-        stub = "pharmaverse",
-        label = i,
-        logo = "static/images/logo-14-14.png",
-        color = "blue",
-        filename = glue("{dir_shields}/{i}")
-      )
-    }
-  }
-  
-  # Summarise badges
-  all_badges <- gsub(".svg","",list.files(dir_shields, pattern = "*.svg"))
-  
-  tibble(
-    Package = all_badges,
-    Shield = glue('<img src="http://pharmaverse.org/shields/{all_badges}.svg">'),
-    Markdown = glue('[<img src="http://pharmaverse.org/shields/{all_badges}.svg">](https://pharmaverse.org)')
-    ) %>%
-    gt() %>%
-    fmt_markdown(columns = Shield) %>%
-    gtsave(filename = "layouts/shortcodes/shields.html")
-  
-
-
-
-
-
-## Internal badges ---------
-
-badges <- read_csv(
-  "https://openpharma.s3.us-east-2.amazonaws.com/badges.csv",
-  na = " "
-  ) %>%
-  mutate(
-    name = basename(full_name)
-  )
+## Get ---------
 
 # helper
   write_badge <- function(
     badge, file, text
   ){
     badge <- badge[1] #edge case issue - not a good hack
+    if(is.na(badge)) return()
     if (!rlang::is_empty(badge)) {
     if (!is.na(badge) & badge != "" & badge != "NA") {
       cat(
@@ -114,25 +29,22 @@ badges <- read_csv(
     }}
   }
 
-
   
   # write new line
-  for (i in files){
+  for (i in files_pharmaverse){
     message(paste("i:",i))
     
     i_package <- gsub(pattern = ".yaml",replacement = "",basename(i))
     
     i_badge_cran <- badges %>% filter(name == i_package) %>% pull(badge_cran)
-    i_badge_health <- badges %>% filter(name == i_package) %>% pull(badge_health)
+    i_badge_stars <- badges %>% filter(name == i_package) %>% pull(badge_stars)
     i_badge_contributors <- badges %>% filter(name == i_package) %>% pull(badge_contributors)
-    i_badge_riskmetric <- badges %>% filter(name == i_package) %>% pull(badge_riskmetric)
     
     message(paste("i_badge_cran:",i_badge_cran))
     
     write_badge(i_badge_cran, i, "badge_cran: ")
-    write_badge(i_badge_health, i, "badge_health: ")
+    write_badge(i_badge_stars, i, "badge_stars: ")
     write_badge(i_badge_contributors, i, "badge_contributors: ")
-    write_badge(i_badge_riskmetric, i, "badge_riskmetric: ")
   }
 
 
