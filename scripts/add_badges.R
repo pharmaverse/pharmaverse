c4207a1c3c1eb7a1af0ac8287a1102cc3a7b6c23
Sankhadeep Dutta <dutta.shankhadeep@gmail.com>
2024-11-08 18:57:25 +0530

    fix: fix logic

--- scripts/add_badges.R
+++ scripts/add_badges.R
@@ -12,42 +12,45 @@ files_pharmaverse <- paste0("data/packages/",list.files("data/packages"))
 
 badges <- arrow::read_parquet(
   "https://openpharma.github.io/generate_badges/output/data.parquet"
-  )
+)
 
 ## Misc ---------
 
 # helper
-  write_badge <- function(
-    badge, file, text
-  ){
-    badge <- badge[1] #edge case issue - not a good hack
-    if(is.na(badge)) return()
-    if (!rlang::is_empty(badge)) {
-    if (!is.na(badge) & badge != "" & badge != "NA") {
-      cat(
-        paste0(text,badge,"\n"),
-        file = file, append = TRUE)   
-    }}
+write_badge <- function(badge, file, text) {
+  # Load the YAML file
+  yaml_data <- yaml::read_yaml(file)
+  
+  # Check if the key is available
+  if (text %in% names(yaml_data)) {
+    # Update the value
+    yaml_data[[text]] <- badge
+  } else {
+    # Append the new key-value pair
+    yaml_data[[text]] <- badge
   }
+  
+  # Write the updated YAML data back to the file
+  yaml::write_yaml(yaml_data, file)
+}
 
 
-# Create data for badges  
-  # write new line
-  for (i in files_pharmaverse){
-    message(paste("i:",i))
-    
-    i_package <- gsub(pattern = ".yaml",replacement = "",basename(i))
-    
-    i_badge_cran <- badges %>% filter(name == i_package) %>% pull(badge_cran)
-    i_badge_stars <- badges %>% filter(name == i_package) %>% pull(badge_stars)
-    i_badge_contributors <- badges %>% filter(name == i_package) %>% pull(badge_contributors)
-    
-    message(paste("i_badge_cran:",i_badge_cran))
-    
-    write_badge(i_badge_cran, i, "badge_cran: ")
-    write_badge(i_badge_stars, i, "badge_stars: ")
-    write_badge(i_badge_contributors, i, "badge_contributors: ")
-  }
+# Create data for badges
+for (i in files_pharmaverse) {
+  message(paste("i:", i))
+  
+  i_package <- gsub(pattern = ".yaml", replacement = "", basename(i))
+  
+  i_badge_cran <- badges %>% filter(name == i_package) %>% pull(badge_cran)
+  i_badge_stars <- badges %>% filter(name == i_package) %>% pull(badge_stars)
+  i_badge_contributors <- badges %>% filter(name == i_package) %>% pull(badge_contributors)
+  
+  message(paste("i_badge_cran:", i_badge_cran))
+  
+  write_badge(i_badge_cran, i, "badge_cran")
+  write_badge(i_badge_stars, i, "badge_stars")
+  write_badge(i_badge_contributors, i, "badge_contributors")
+}
 
 # Make physical badges ----
 
